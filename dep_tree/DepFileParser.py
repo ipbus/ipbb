26eda333dcb8827c28c06e732160113c2cb95899
Alessandro Thea <alessandro.thea@cern.ch>
2016-11-07 08:22:48 +0100

    toolset selection aligned with command targets

--- dep_tree/DepFileParser.py
+++ dep_tree/DepFileParser.py
@@ -17,9 +17,9 @@ class Command(object):
 
   def __str__(self):
     return str(self.__dict__)
-  
-  def __eq__(self, other): 
-    return (self.FilePath == other.FilePath) and (self.Lib == other.Lib)   
+
+  def __eq__(self, other):
+    return (self.FilePath == other.FilePath) and (self.Lib == other.Lib)
   #--------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -34,7 +34,7 @@ class DepFileParser(object):
     self.CommandLineArgs = aCommandLineArgs
     self.Pathmaker = aPathmaker
     self.depth = 0
-    
+
     self.ScriptVariables = {}
     self.ComponentPaths = list()
     self.CommandList = {"setup": [], "src": [], "addrtab": [] , "cgpfile" : [] }
@@ -42,26 +42,26 @@ class DepFileParser(object):
     self.Maps = list()
     self.FilesNotFound = list()
     #--------------------------------------------------------------
-    
+
     #--------------------------------------------------------------
     # Add to or override the Script Variables with user commandline
     for lArgs in self.CommandLineArgs.define:
       lKey , lVal = lArgs.split('=')
       self.ScriptVariables[ lKey ] = lVal
     #--------------------------------------------------------------
-    
+
     #--------------------------------------------------------------
     # Set the toolset
-    if self.CommandLineArgs.product == 'x':
+    if self.CommandLineArgs.product == 'xtclsh':
       self.ScriptVariables[ "toolset" ] = "ISE"
-    elif self.CommandLineArgs.product == 'v':
+    elif self.CommandLineArgs.product == 'vivado':
       self.ScriptVariables[ "toolset" ] = "Vivado"
-    elif self.CommandLineArgs.product == 's' or self.CommandLineArgs.product == 'ip':
+    elif self.CommandLineArgs.product == 'sim' or self.CommandLineArgs.product == 'ip':
       self.ScriptVariables[ "toolset" ] = "Modelsim"
     else:
-      self.ScriptVariables[ "toolset" ] = "other"      
+      self.ScriptVariables[ "toolset" ] = "other"
     #--------------------------------------------------------------
-    
+
     #--------------------------------------------------------------
     # Set up the parser
     parser = argparse.ArgumentParser(usage = argparse.SUPPRESS)
@@ -101,7 +101,7 @@ class DepFileParser(object):
     self.parseLine = parser.parse_args
     #--------------------------------------------------------------
   #----------------------------------------------------------------------------------------------------------------------------
-  
+
   #----------------------------------------------------------------------------------------------------------------------------
   def parse(self, aFileName, aComponentPath):
 
@@ -109,7 +109,7 @@ class DepFileParser(object):
     # We have gone one layer further down the rabbit hole
     self.depth += 1
     #--------------------------------------------------------------
-    
+
     #--------------------------------------------------------------
     # Open the dep file and iterate over it
     with open(aFileName) as lDepFile:
@@ -120,7 +120,7 @@ class DepFileParser(object):
         # Ignore blank lines and comments
         if lLine == "" or lLine[0] == "#": continue
         #--------------------------------------------------------------
-              
+
         #--------------------------------------------------------------
         # Process the assignment directive
         if lLine[0] == "@":
@@ -136,33 +136,33 @@ class DepFileParser(object):
               raise SystemExit( "Parsing directive failed in {0} , line '{1}'".format( aFileName , lLine ) )
           continue
         #--------------------------------------------------------------
-        
+
         #--------------------------------------------------------------
         # Process the conditional directive
         if lLine[0] == "?":
           lTokens = [ i for i, letter in enumerate(lLine) if letter == "?" ]
           if len(lTokens) != 2:
             raise SystemExit( "There must be precisely two '?' tokens per line. Found {0} in {1} , line '{2}'".format( len(lTokens) , aFileName , lLine ) )
-          
+
           try:
             lExprValue = eval( lLine[ lTokens[0]+1 : lTokens[1] ] , None , self.ScriptVariables )
           except:
             raise SystemExit( "Parsing directive failed in {0} , line '{1}'".format( aFileName , lLine ) )
-                  
+
           if not isinstance( lExprValue , bool ):
             raise SystemExit( "Directive does not evaluate to boolean type in {0} , line '{1}'".format( aFileName , lLine ) )
-                  
+
           if not lExprValue:
             continue
-          
-          lLine = lLine[ lTokens[1]+1 : ].strip() # if line is accepted, strip the conditionality from the front and carry on 
+
+          lLine = lLine[ lTokens[1]+1 : ].strip() # if line is accepted, strip the conditionality from the front and carry on
         #--------------------------------------------------------------
-        
+
         #--------------------------------------------------------------
         # Parse the line using arg_parse
         lParsedLine = self.parseLine(lLine.split())
         #--------------------------------------------------------------
-  
+
         #--------------------------------------------------------------
         # Throw on depricated syntax
         if lParsedLine.descend or lParsedLine.subdir:
@@ -170,41 +170,41 @@ class DepFileParser(object):
             lParsedLine.descend = 0
           if lParsedLine.subdir == None:
             lParsedLine.subdir = ""
-                      
+
           if len( lParsedLine.file ) == 1:
             cmd = "{0} ...flags... {1}".format( lParsedLine.cmd, os.path.normpath( os.path.join( "../"*lParsedLine.descend , lParsedLine.subdir, lParsedLine.file[0]  )) )
           else:
-            cmd = "{0} ...flags... --cd {1} {2}".format( lParsedLine.cmd, os.path.normpath( os.path.join( "../"*lParsedLine.descend , lParsedLine.subdir )) , " ".join( lParsedLine.file ) ) 
-            
+            cmd = "{0} ...flags... --cd {1} {2}".format( lParsedLine.cmd, os.path.normpath( os.path.join( "../"*lParsedLine.descend , lParsedLine.subdir )) , " ".join( lParsedLine.file ) )
+
           raise SystemExit( "Dep file --descend (-d) and -subdir (-s) syntax is deprecated , in {0} please change line\n '{1}' -> '{2}'".format( aFileName , lLine , cmd ) )
         #--------------------------------------------------------------
-  
+
         #--------------------------------------------------------------
         # Set the component path, whether specified explicitly or not
         if (lParsedLine.component is None):
-          lComponentPath = aComponentPath    
+          lComponentPath = aComponentPath
         else:
           lComponentPath = lParsedLine.component
           if lComponentPath not in self.ComponentPaths:
             self.ComponentPaths.append(lComponentPath)
         #--------------------------------------------------------------
-                
+
         #--------------------------------------------------------------
-        # Set the target file expression, whether specified explicitly or not       
+        # Set the target file expression, whether specified explicitly or not
         if (not lParsedLine.file):
           lComponentName = lComponentPath.split('/')[-1]
           lFileExprList = [ self.Pathmaker.getdefname( lParsedLine.cmd , lComponentName ) ]
         else:
           lFileExprList = lParsedLine.file
         #--------------------------------------------------------------
-        
+
         #--------------------------------------------------------------
-        # Set the target library, whether specified explicitly or not 
+        # Set the target library, whether specified explicitly or not
         if ('lib' in lParsedLine) and (lParsedLine.lib):
           lLib = lParsedLine.lib
           self.Libs.append(lLib)
         else:
-          lLib = None  
+          lLib = None
         #--------------------------------------------------------------
 
         #--------------------------------------------------------------
@@ -216,13 +216,13 @@ class DepFileParser(object):
         #--------------------------------------------------------------
 
         #--------------------------------------------------------------
-        # Set some processing flags, whether specified explicitly or not 
+        # Set some processing flags, whether specified explicitly or not
         if 'noinclude' in lParsedLine:
           lInclude = not lParsedLine.noinclude
         else:
           lInclude = True
-          
-        if 'toplevel' in lParsedLine:  
+
+        if 'toplevel' in lParsedLine:
           lTopLevel = lParsedLine.toplevel
         else:
           lTopLevel = False
@@ -238,13 +238,13 @@ class DepFileParser(object):
 
         #--------------------------------------------------------------
         # Debugging
-        if self.CommandLineArgs.verbosity > 1: 
+        if self.CommandLineArgs.verbosity > 1:
           print("***", lParsedLine, lComponentPath, lFileExprList)
         #--------------------------------------------------------------
-               
+
         for lFileExpr in lFileExprList:
           #--------------------------------------------------------------
-          # If we are looking at generated files, look in the ipcore_dir, else look where we are told          
+          # If we are looking at generated files, look in the ipcore_dir, else look where we are told
           if 'generated' in lParsedLine and lParsedLine.generated:
             lPath = self.Pathmaker.getrelpath( "ipcore_dir" , lFileExpr , lParsedLine.cd )
             lFileList = [ lPath ]
@@ -252,20 +252,20 @@ class DepFileParser(object):
             lPath = self.Pathmaker.getpath( lComponentPath , lParsedLine.cmd , lFileExpr , cd = lParsedLine.cd )
             lFileList = glob.glob( lPath )
           #--------------------------------------------------------------
-  
+
           #--------------------------------------------------------------
           # Warn if something looks odd
           if not lFileList:
             self.FilesNotFound.append( lPath )
           #--------------------------------------------------------------
-  
-          for lFile in lFileList:    
+
+          for lFile in lFileList:
             #--------------------------------------------------------------
             # Debugging
-            if self.CommandLineArgs.verbosity > 0: 
+            if self.CommandLineArgs.verbosity > 0:
               print("  " * self.depth, lParsedLine.cmd, lFileExpr, lFile, os.path.exists(lFile))
             #--------------------------------------------------------------
-              
+
             #--------------------------------------------------------------
             # If an include command, parse the specified dep file, otherwise add the command to the command list
             if lParsedLine.cmd == "include":
@@ -277,19 +277,19 @@ class DepFileParser(object):
                 lMap = lParsedLine.map
                 self.Maps.append((lMap, lFile))
               else:
-                lMap = None                
+                lMap = None
 
               self.CommandList[ lType ].append( Command( lFile, lLib, lMap, lInclude , lTopLevel , lComponentPath, lVhdl2008 ) )
             #--------------------------------------------------------------
-              
+
 
     #--------------------------------------------------------------
-           
+
     #--------------------------------------------------------------
     # We are about to return one layer up the rabbit hole
     self.depth -= 1
     #--------------------------------------------------------------
-    
+
     #--------------------------------------------------------------
     # If we are exiting the top-level, uniquify the commands list, keeping the order as defined in Dave's origianl voodoo
     if self.depth==0:
@@ -299,7 +299,7 @@ class DepFileParser(object):
           if not j in lTemp:
             lTemp.append(j)
         lTemp.reverse()
-        self.CommandList[i] = lTemp    
+        self.CommandList[i] = lTemp
     #--------------------------------------------------------------
   #----------------------------------------------------------------------------------------------------------------------------
 

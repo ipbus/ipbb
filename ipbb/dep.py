cb1c8bfef02896718f9dba3ed011f6201820b590
Alessandro Thea <alessandro.thea@cern.ch>
2016-11-28 14:22:07 +0100

    Simplified dep tree interface in ipbb

--- ipbb/dep.py
+++ ipbb/dep.py
@@ -3,20 +3,11 @@ from __future__ import print_function
 # Modules
 import click
 
-#------------------------------------------------------------------------------
-@click.group()
-def dep():
-  pass
-#------------------------------------------------------------------------------
+from dep_tree.SmartOpen import SmartOpen
+
 
 #------------------------------------------------------------------------------
-@dep.command()
-@click.option('-o', '--output', default=None)
-@click.pass_obj
-def addrtab( env, output ):
-
-  #------------------------------------------------------------------------------
-  # Very messy, to be sorted out later
+def _makeParser(env):
   from dep2g.Pathmaker import Pathmaker
   from dep2g.DepFileParser import DepFileParser
 
@@ -33,11 +24,97 @@ def addrtab( env, output ):
   lDepFileParser = DepFileParser( lCommandLineArgs , lPathmaker )
   lDepFileParser.parse(lCfg['topPkg'], lCfg['topCmp'], lCfg['topDep'])
 
+  return lDepFileParser, lPathmaker, lCommandLineArgs
+
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+@click.group()
+def dep():
+  pass
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+@dep.command()
+@click.option('-o', '--output', default=None)
+@click.pass_obj
+def dump( env, output ):
+  '''List source files'''
+  lDepFileParser, lPathmaker, lCommandLineArgs = _makeParser(env)
+
+  with SmartOpen( output ) as lWriter:
+    lWriter( str(lDepFileParser) )
+#------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
+@dep.command()
+@click.argument('group',type=click.Choice(['setup', 'src', 'addrtab', 'cgpfile']))
+@click.option('-o', '--output', default=None)
+@click.pass_obj
+def cmds( env, group, output ):
+  '''List source files'''
+  lDepFileParser, lPathmaker, lCommandLineArgs = _makeParser(env)
+
+  with SmartOpen( output ) as lWriter:
+    for addrtab in lDepFileParser.CommandList[group]:
+      lWriter( addrtab.FilePath )
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+@dep.command()
+@click.option('-o', '--output', default=None)
+@click.pass_obj
+def addrtab( env, output ):
+  '''List address table files'''
+
+  lDepFileParser, lPathmaker, lCommandLineArgs = _makeParser(env)
+
   from dep2g.AddressTableListMaker import AddressTableListMaker
 
-  from dep_tree.SmartOpen import SmartOpen
+  with SmartOpen( output ) as lWriter:
+    for addrtab in lDepFileParser.CommandList["addrtab"]:
+      lWriter( addrtab.FilePath )
+#------------------------------------------------------------------------------
 
-  with SmartOpen( output ) as lTarget:
-    AddressTableListMaker(lCommandLineArgs, lPathmaker).write(lTarget,lDepFileParser.ScriptVariables, lDepFileParser.Components, lDepFileParser.CommandList, None, None)
+#------------------------------------------------------------------------------
+@dep.command()
+@click.option('-o', '--output', default=None)
+@click.pass_obj
+def sources( env, output ):
+  '''List source files'''
 
+  lDepFileParser, lPathmaker, lCommandLineArgs = _makeParser(env)
+
+  with SmartOpen( output ) as lWriter:
+    for addrtab in lDepFileParser.CommandList["src"]:
+      lWriter( addrtab.FilePath )
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+@dep.command()
+@click.option('-o', '--output', default=None)
+@click.pass_obj
+def components( env, output ):
+
+  lDepFileParser, lPathmaker, lCommandLineArgs = _makeParser(env)
+
+  with SmartOpen( output ) as lWriter:
+    for lPkt, lCmps in lDepFileParser.Components.iteritems():
+      lWriter('['+lPkt+']')
+      for lCmp in lCmps:
+        lWriter(lCmp)
+      lWriter()
+
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+@dep.command()
+@click.pass_obj
+def ipy( env ):
+  '''Opens IPython to inspect the parser'''
+
+  lDepFileParser, lPathmaker, lCommandLineArgs = _makeParser(env)
+
+  import IPython
+  IPython.embed()
 #------------------------------------------------------------------------------
---
+++ ipbb/sim.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+#------------------------------------------------------------------------------
+
+# Modules
+import click
+import os
+import ipbb
+# Elements
+from os.path import join, split, exists, splitext
+from .common import which

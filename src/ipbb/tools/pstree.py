739f7491f4af101d807a4856b9f4750895f9d93c
Alessandro Thea <alessandro.thea@cern.ch>
2019-02-27 12:11:22 +0100

    Working on process monitoring

--- src/ipbb/tools/pstree.py
+++ src/ipbb/tools/pstree.py
@@ -29,7 +29,7 @@ class ProcessIter(object):
         # The first non-0 becomes the current
         self.current = lNextNode
 
-        d = len(self.stack) - 1
+        # d = len(self.stack) - 1
 
         # sort next out
         lChildren = lNextNode.children
@@ -37,7 +37,6 @@ class ProcessIter(object):
             self.stack.append(iter(lChildren))
 
         return self.current.process
-# -----------------------------------------------------------------------------
 
 
 # -----------------------------------------------------------------------------
@@ -57,7 +56,6 @@ class ProcessNode(object):
 
     def __repr__(self):
         return 'ProcessNode(%d)' % self.process.pid
-# -----------------------------------------------------------------------------
 
 
 # -----------------------------------------------------------------------------
@@ -89,7 +87,18 @@ class ProcessTree(object):
 
     def __iter__(self):
         return ProcessIter(self.headnode)
+
+
 # -----------------------------------------------------------------------------
+class ProcessTreeSnapshot(object):
+    """docstring for ProcessTreeSnapshot
+    
+    Attributes:
+        arg (TYPE): Description
+    """
+    def __init__(self, arg):
+        super(ProcessTreeSnapshot, self).__init__()
+        self.arg = arg
 
 
 # -----------------------------------------------------------------------------
@@ -99,11 +108,14 @@ class ProcessTreeAnalyzer(object):
     Attributes:
         process (obj:`psutil.Process`): Description
     """
-    def __init__(self, process, fields=['memory_full_info', 'cpu_percent', 'num_threads', 'cpu_times']):
+
+    # -----------------------------------------------------
+    def __init__(self, process, fields=['name', 'cmdline', 'memory_full_info', 'cpu_percent', 'num_threads', 'cpu_times']):
         super(ProcessTreeAnalyzer, self).__init__()
         self.process = process
         self.fields = fields
 
+    # -----------------------------------------------------
     def snapshot(self, aInterval=None):
         lProcs = [self.process] + self.process.children(True)
 
@@ -114,13 +126,20 @@ class ProcessTreeAnalyzer(object):
 
             time.sleep(aInterval)
 
-        aData = []
+        lData = []
         for p in lProcs:
             with p.oneshot():
-                aData += [[getattr(p, x)() for x in self.fields]]
+                lData += [ (p.pid, [getattr(p, x)() for x in self.fields])]
+
+        return lData
+
+    # -----------------------------------------------------
+    def summary(self, aInterval=None):
+
+        lData = self.snapshot(aInterval)
 
         lSums = []
-        for row in zip(*aData):
+        for row in zip(*lData):
             if isinstance(row[0], tuple):
                 lCls = type(row[0])
                 lSums.append(lCls._make(map(sum, zip(*row))))
@@ -128,4 +147,3 @@ class ProcessTreeAnalyzer(object):
                 lSums.append(sum(row))
 
         return zip(self.fields, lSums)
-# ------------------------------------------------------------------------------
--- tests/scripts/test_psiter.py
+++ tests/scripts/test_psiter.py
@@ -6,7 +6,8 @@ import time
 import collections
 import itertools
 
-from ipbb.tools.pstree import ProcessTree, ProcessIter, ProcessNode, ProcessAnalyzer
+from ipbb.tools.pstree import ProcessTree, ProcessIter, ProcessNode, ProcessTreeAnalyzer
+from texttable import Texttable
 
 
 if len(sys.argv) != 2:
@@ -24,11 +25,10 @@ def test_iter(pid):
 
     for i in z:
         pass
-# -----------------------------------------------------------------------------
 
 
 # -----------------------------------------------------------------------------
-if __name__ == '__main__':
+def test_summary(pid):
     from collections import defaultdict
 
     p = psutil.Process(pid)
@@ -38,6 +38,53 @@ if __name__ == '__main__':
         print 'Process', i
 
     pa = ProcessTreeAnalyzer(p)
-    for n, v in pa.snapshot(0.1):
-        print n, v
+    print 'stats'
+    s = pa.snapshot(0.1)
+
+    lSummary = Texttable(max_width=0)
+    lSummary.set_deco(Texttable.HEADER | Texttable.BORDER)
+    lSummary.header(['pid'] + pa.fields)
+
+    for pid, data in s:
+        print pid, data
+        lSummary.add_row([pid] + data)
+
+    # for n, v in pa.summary(1):
+        # print n, 
+    print lSummary.draw()
+
+
 # -----------------------------------------------------------------------------
+def test_xxx(pid):
+    p = psutil.Process(pid)
+
+    fields = ['name', 'cmdline', 'memory_full_info', 'cpu_percent', 'num_threads', 'cpu_times.user']
+
+    # Normalise the fields
+    for fn in fields:
+        if '.' in fn:
+            print fn.split('.')
+
+
+    # lData = []
+    # with p.oneshot():
+
+    #     lProps = []
+    #     for f in fields:
+
+    #         x = getattr(p, f)()
+    #         if f is 'cmdline':
+    #             lProps += [' '.join(x)]
+    #         elif isinstance(x, tuple):
+    #             lProps += [e for e in x]
+    #         else:
+    #             lProps += [x]
+
+    #     lData += [(p.pid, lProps)]
+    #     lData += [ (p.pid, [getattr(p, x)() for x in fields])]
+
+    # print lData
+
+# -----------------------------------------------------------------------------
+if __name__ == '__main__':
+    test_xxx(pid)

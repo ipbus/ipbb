965fd78a4b4c4c181536e001bb20b76826508ab2
Alessandro Thea <alessandro.thea@cern.ch>
2020-07-08 15:20:31 +0200

    Small tweaks here and there.

--- sandbox/report_timing.tcl
+++ sandbox/report_timing.tcl
@@ -1,2 +1,4 @@
 report_timing -delay_type min_max -max_paths 10 -slack_less_than 0 -sort_by group -input_pins -name timing_3
 
+set failing_paths [get_timing_paths -delay_type min_max -max_paths 10 -slack_less_than 0 -sort_by group]
+report_property [lindex $failing_paths 0]

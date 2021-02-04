a8847298708482a208b6b0eb0d687a55491af5ed
Alessandro Thea <alessandro.thea@cern.ch>
2021-02-04 23:48:12 +0100

    Command completion test code moved into tests.

--- src/ipbb/scripts/builder.py
+++ src/ipbb/scripts/builder.py
@@ -38,10 +38,6 @@ def climain(ctx, aExcStack):
 
 
 # ------------------------------------------------------------------------------
-
-
-
-# ------------------------------------------------------------------------------
 def _compose_cli():
     # Add custom cli to shell
     from ..cli import repo
@@ -80,7 +76,7 @@ def _compose_cli():
     climain.add_command(sim.sim)
 
     from ..cli import vivadohls
-    
+
     vivadohls.vivadohls.add_command(common.cleanup)
     climain.add_command(vivadohls.vivadohls)
 
@@ -102,49 +98,6 @@ def main():
         raise SystemExit(-1)
 
     _compose_cli()
-
-    # if True:
-    if False:
-        from click._bashcomplete import get_choices
-
-        def choices_without_help(cli, args, incomplete):
-            completions = get_choices(cli, 'dummy', args, incomplete)
-            return [c[0] for c in completions]
-
-        for inc in [
-                '',
-                'f',
-                'felix-pie',
-                'felix-pie:',
-                'felix-pie:p',
-                'felix-pie:projects/',
-                'felix-pie:projects/hi',
-                'felix-pie:projects/hitfinder/'
-        ]:
-            print("-" * 80)
-            print("Completing component'" + inc + "'")
-            print("-" * 80)
-            print(choices_without_help(climain, ['proj', 'create', 'vivado', 'jbsc-hf-fc-tightG'], inc))
-            print()
-
-        for inc in [
-                '',
-        ]:
-            print("-" * 80)
-            print("Completing dep file'" + inc + "'")
-            print("-" * 80)
-            print(choices_without_help(climain, ['ipbb', 'toolbox', 'check-dep', 'felix-pie:projects/hitfinder'], inc))
-            print()
-
-        for inc in [
-                '',
-        ]:
-            print("-" * 80)
-            print("Completing dep file'" + inc + "'")
-            print("-" * 80)
-            print(choices_without_help(climain, ['proj', 'create', 'vivado', 'jbsc-hf-fc-tightG', 'felix-pie:projects/hitfinder', '-t'], inc))
-            print()
-        raise SystemExit(0)
 
     obj = Environment()
     try:

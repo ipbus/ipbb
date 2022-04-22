c1500aa1a1ac290c083f8ccb91866de4bcdc2224
Alessandro Thea <alessandro.thea@cern.ch>
2022-04-22 17:26:10 +0200

    Fixing tests

--- tests/pytests/test_depparser.py
+++ tests/pytests/test_depparser.py
@@ -24,18 +24,18 @@ def dep_parser():
 def test_no_assign(dep_parser):
     # Lines without a @ are not processed
     line_no_assignment = 'a = "b"'
-    assert dep_parser._lineProcessAssignments(line_no_assignment) == line_no_assignment
+    assert dep_parser._line_process_assignments(line_no_assignment) == line_no_assignment
 
 
 # -----------------------------------------------------------------------------
 def test_assign(dep_parser):
-    assert dep_parser._lineProcessAssignments('@ a = "a_value"') == None
+    assert dep_parser._line_process_assignments('@ a = "a_value"') == None
     assert dep_parser.settings['a'] == 'a_value'
-    assert dep_parser._lineProcessAssignments('@ c = a') == None
+    assert dep_parser._line_process_assignments('@ c = a') == None
     assert dep_parser.settings['c'] == 'a_value'
-    assert dep_parser._lineProcessAssignments('@ d.e.f = 3.4') == None
+    assert dep_parser._line_process_assignments('@ d.e.f = 3.4') == None
     assert dep_parser.settings['d.e.f'] == 3.4
-    assert dep_parser._lineProcessAssignments('@ hls.cflags = "-std=c++11"') == None
+    assert dep_parser._line_process_assignments('@ hls.cflags = "-std=c++11"') == None
     assert dep_parser.settings['hls.cflags'] == "-std=c++11"
 
 
@@ -43,29 +43,29 @@ def test_assign(dep_parser):
 # -----------------------------------------------------------------------------
 def test_assign_invalid_expr(dep_parser):
     with pytest.raises(DepAssignmentError):
-        dep_parser._lineProcessAssignments('@ hello world')
+        dep_parser._line_process_assignments('@ hello world')
 
 
 # -----------------------------------------------------------------------------
 def test_assign_no_parname(dep_parser):
     with pytest.raises(DepAssignmentError):
-        dep_parser._lineProcessAssignments('@ = s')
+        dep_parser._line_process_assignments('@ = s')
 
 
 # -----------------------------------------------------------------------------
 def test_assign_no_value(dep_parser):
     with pytest.raises(DepAssignmentError):
-        dep_parser._lineProcessAssignments('@ x =')
+        dep_parser._line_process_assignments('@ x =')
 
 
 # -----------------------------------------------------------------------------
 def test_assign_invalid_parname(dep_parser):
     with pytest.raises(DepAssignmentError):
-        dep_parser._lineProcessAssignments('@ 2_not_good = 0')
+        dep_parser._line_process_assignments('@ 2_not_good = 0')
 
 
 # -----------------------------------------------------------------------------
 def test_assign_invalid_value(dep_parser):
 
-    dep_parser._lineProcessAssignments('@ a = print(3)')
+    dep_parser._line_process_assignments('@ a = print(3)')
 
